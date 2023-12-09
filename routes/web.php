c0ca78243359e098487c883ff28e1d305f3e2578
Hancie123 <hanciewanemphago@gmail.com>
2023-12-09 15:44:02 +0545

    login  ui added

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',[HomeController::class,'index']);
+
+Route::get('/login',[AuthController::class,'index']);
+
+Route::get('/login/forgot-password', [AuthController::class, 'forgotPasswordIndex']);
