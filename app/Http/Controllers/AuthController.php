166a026005cd8ba2d19ba693d6762b3c4e19f710
Hancie123 <hanciewanemphago@gmail.com>
2023-12-09 16:13:18 +0545

    login auth controller added

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -14,7 +16,29 @@ class AuthController extends Controller
         return view('forgot_password');
     }
 
-    public function login(){
+    public function login(LoginRequest $request){
+        
+        $credentials=$request->only('email','password');
+
+        try{
+            if(Auth::attempt($credentials)){
+                $user=Auth::user();
+                return back()->with('success','login success');
+                
+                
+            }else{
+                sweetalert()->addWarning("We couldn't verify your credentials. Please 
+                check your email and password!");
+                return back();
+            }
+            
+        }
+        catch(\Throwable $t){
+            return back()->with('error',$t->getMessage());
+            
+        }
+
+        
         
     }
 }
--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -19,7 +20,8 @@ class AdminSeeder extends Seeder
             'email' => 'hanciephago@gmail.com',
             'password' => Hash::make('password'),
             'address'=>'Kathmandu',
-            'mobile_no'=>'9825915122'
+            'mobile_no'=>'9825915122',
+            'email_verified_at'=>Carbon::now()
             
         ];
 
