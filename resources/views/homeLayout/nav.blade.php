899f0855ccee491112db96e350c7f64f62d75f0b
Hancie123 <hanciewanemphago@gmail.com>
2023-12-25 20:32:57 +0545

    nav updated

--- resources/views/homeLayout/nav.blade.php
+++ resources/views/homeLayout/nav.blade.php
@@ -7,9 +7,11 @@
         <nav id="navbar" class="navbar order-last order-lg-0">
             <ul>
                 <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-                <li><a class="nav-link scrollto" href="#about">About</a></li>
-                <li><a class="nav-link scrollto" href="#services">Lottery Results</a></li>
-                <li><a href="blog.html">Blog</a></li>
+                <li><a class="nav-link scrollto" href="">1:00 PM Results</a></li>
+                <li><a class="nav-link scrollto" href="">6:00 PM Results</a></li>
+                <li><a class="nav-link scrollto" href="">8:00 PM Results</a></li>
+                <li><a class="nav-link scrollto" href="#services">Today Results</a></li>
+                <li><a class="nav-link scrollto" href="#services">Old Results</a></li>
                 <li><a class="nav-link scrollto" href="{{url('contact-us')}}">Contact</a></li>
             </ul>
             <i class="bi bi-list mobile-nav-toggle"></i>
