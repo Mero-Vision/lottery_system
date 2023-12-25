9409ad9ba579dbe50a38e03d639baa0e81b26b48
Hancie123 <hanciewanemphago@gmail.com>
2023-12-25 19:35:31 +0545

    lottery added

--- app/Models/Lottery.php
+++ app/Models/Lottery.php
@@ -6,9 +6,19 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Lottery extends Model implements HasMedia
 {
     use HasFactory;
     use InteractsWithMedia;
+    use HasSlug;
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('lottery_name')
+            ->saveSlugsTo('slug');
+    }
 }
