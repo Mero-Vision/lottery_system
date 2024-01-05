782c8abdd689dd211d8451da5092d83d8542b4eb
Hancie123 <hanciewanemphago@gmail.com>
2024-01-05 21:59:58 +0545

    ui updated

--- resources/views/homeLayout/footer.blade.php
+++ resources/views/homeLayout/footer.blade.php
@@ -54,7 +54,7 @@
 
             <div class="me-md-auto text-center text-md-start">
                 <div class="copyright">
-                    &copy; Copyright <strong><span>Tech Revo</span></strong>. All Rights Reserved
+                    &copy; Copyright <strong><span>Royal Lucky (10)</span></strong>. All Rights Reserved
                 </div>
                 {{-- <div class="credits">
                     Designed by <a href="#">Hancie Phago and Nitesh Hamal</a>
